4973f7f33abc190dd087470e2a00954241bdbc11
Ganesh Singh <ganeshsingh757@yourdomain.com>
2023-01-31 15:51:17 +0530

    added the postgres db image in docker-compose and to the django app.

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    """Adding two numbers
+
+    Args:
+        x (int): first input
+        y (int): second input
+    """
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
