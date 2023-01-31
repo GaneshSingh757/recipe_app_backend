682442c6253de88b84942125a1f72761856f8983
Ganesh Singh <ganeshsingh757@gmail.com>
2023-01-31 16:01:25 +0530

    modified for flake8 linting issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,6 +7,7 @@ def add(x, y):
     """
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,12 +4,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
-    def  test_add_numbers(self):
+    def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
